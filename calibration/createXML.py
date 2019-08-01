193b37ffea1b3d26294b93799a07531509dc7649
Danish Shabbir <danishs@stanford.edu>
2019-08-01 15:15:34 -0700

    comments

--- calibration/createXML.py
+++ calibration/createXML.py
@@ -1,10 +1,11 @@
-# Authored by Sam Kwong
+# Authored by Sam Kwong on July 30, 2019
 
 import os
 import sys
 import argparse
 import numpy as np
 import xml.etree.ElementTree as ET
+from xml.dom.minidom import parse, parseString
 import ast
 
 def cleanFormats(rotMat, transMat):
@@ -25,7 +26,7 @@ if __name__ == '__main__':
     f_list = sorted([os.fsdecode(f) for f in os.listdir(os.fsencode(opt.output_folder)) if os.fsdecode(f).endswith('.xml')])
     extMatStrs = [0] * len(f_list)
 
-    # create an extrinsic parameters matrix for each camera
+    # ------------------------------- create an extrinsic parameters matrix for each camera ---------------------------------
     print('Retrieving camera extrinsic parameters... (' + opt.input_path + ')')
     info = open(opt.input_path, 'r')
     txt = info.read()
@@ -34,30 +35,31 @@ if __name__ == '__main__':
 
     for i in range(len(f_list)):
         rotMatStr, transMatStr = rotMatStrs[i+1], transMatStrs[i+1] # get what comes after '['
+        # disregard ']'
         if i < len(f_list)-1:
             rotMatStr, transMatStr = rotMatStr.replace(']', ''), transMatStr.replace(']', '')
         else:
             rotMatStrEndIdx, transMatStrEndIdx = rotMatStr.find(']'), transMatStr.find(']')
             rotMatStr, transMatStr = rotMatStr[:rotMatStrEndIdx], transMatStr[:transMatStrEndIdx]
-        print(rotMatStr, transMatStr)
+        
         rotMatList, transMatList = ast.literal_eval('[' + rotMatStr + ']'), ast.literal_eval('[' + transMatStr + ']')
-
         rotMatFormatted, transMatFormatted = cleanFormats(rotMatList, transMatList)
         extMat = createExtMat(rotMatFormatted, transMatFormatted)
         extMatStrs[i] = np.array2string(extMat).strip('[]').replace('  ', ' ')
     
-    # insert the appropriate extrinsic parameters matrix into each camera's XML file
-    for i,f in enumerate(f_list): 
+    # ------------------------------- insert the appropriate extrinsic parameters matrix into each camera's XML file ----------------------------------
+    for i,f in enumerate(f_list):
         tree = ET.parse(opt.output_folder + '/' + f)
         root = tree.getroot()
         for elem in root.findall('./CameraMatrix/data'):
             elem.text = extMatStrs[i]
-        treeStr = ET.tostring(root, method='xml')
         
-        #xml_str = "<?xml version=\"1.0\" encoding=\"%s\"?>" + sys.stdout.write(newline) + treeStr
-        tree.write(opt.output_folder + '/' + f)
-        # tree.write(opt.output_folder + '/' + f, encoding="UTF-8", xml_declaration=True)
-        #with open(opt.output_folder + '/' + f, 'wb') as xml_file:
-            #xml_file.write(xml_str)
+        tree.write(opt.output_folder + '/' + f) # using xml_declaration=True will add in xml header with single quotes (codebase requires double quotes)
 
-    
+        # append header as a string at the beginning (in order to keep the double quotes instead of single quotes)
+        xmlHeader = "<?xml version=\"1.0\"?>"
+        xmlStr = xmlHeader + str(ET.tostring(root)).replace('b\'', '').replace('\'', '').replace('\\n', '').replace('  ', ' ')
+        print(xmlStr)
+        with open(opt.output_folder + '/' + f, 'w') as openF:
+            openF.write(xmlStr)
+        
